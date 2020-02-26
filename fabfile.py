be05b3ec1a9fd352dcb03b09bc6cd2cdc40a4726
Antoine Beaupré <anarcat@debian.org>
2020-02-25 19:37:38 -0500

    correctly include ganeti module

--- fabfile.py
+++ fabfile.py
@@ -11,4 +11,4 @@ import fabric_tpa.reboot
 ns = Collection(fabric_tpa.reboot,
                 fabric_tpa.kvm_migrate,
                 fabric_tpa.host_decom,
-                fabric_tpa.reboot)
+                fabric_tpa.ganeti)
