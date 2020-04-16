2c6d232783f52bfd472e4eb15cf9a1cec6f5a897
Antoine Beaupré <anarcat@debian.org>
2020-04-16 14:34:33 -0400

    sort modules in fabfile
    The previous order seemed almost there, only retire was out of order.

--- fabfile.py
+++ fabfile.py
@@ -5,16 +5,16 @@ from invoke import Collection
 
 # get the safe_open hack
 import fabric_tpa  # noqa: F401
-import fabric_tpa.retire
 import fabric_tpa.ganeti
 import fabric_tpa.host
 import fabric_tpa.libvirt
 import fabric_tpa.reboot
+import fabric_tpa.retire
 
 ns = Collection(
-    fabric_tpa.retire,
     fabric_tpa.ganeti,
     fabric_tpa.host,
     fabric_tpa.libvirt,
     fabric_tpa.reboot,
+    fabric_tpa.retire,
 )
