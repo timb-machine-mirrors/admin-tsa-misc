c7c5d9b8ba07e2e2ff2b288128d49320aa51f264
Antoine Beaupré <anarcat@debian.org>
2020-04-27 16:51:08 -0400

    find LDAP admins properly
    They are not in the "adm" group (that's UNIX root), they are
    a *member* of the LDAP administrator *user*.

--- fabric_tpa/user.py
+++ fabric_tpa/user.py
@@ -61,11 +61,18 @@ LDAP_VALID_USERS_FILTER = """
 )
 
 
-def parse_ldap_result_user(dn, result):
+def parse_ldap_result_user(con, dn, result):
     uid = result["uid"][0].decode("utf-8")
     uidNumber = result["uidNumber"][0].decode("utf-8")
     gidNumber = result["gidNumber"][0].decode("utf-8")
+
     groups = [g.decode("utf-8") for g in result["supplementaryGid"]]
+    # also look for membership in other users, this mostly covers the
+    # "LDAP Administrator" "group" (which is really a user, because
+    # it's in "ou=users"!)
+    for dn, result in con.search_users("(member=%s)" % dn):
+        groups.append(result["cn"][0].decode("utf-8"))
+
     groups_str = ",".join(groups)
     flags = list(groups_to_flags(groups))
     # TODO: we should also check the gid resolves to the same group
@@ -79,14 +86,17 @@ def parse_ldap_result_user(dn, result):
 
 def groups_to_flags(groups):
     """parse the list of groups and generate meaningful flags"""
-    if "adm" in groups:
+    if "LDAP administrator" in groups:
         yield "ldap-admin"
+    if "adm" in groups:
+        yield "root-adm"
     if "torproject" in groups:
         yield "login-everywhere"
 
 
 flags_meaning = {
-    "ldap-admin": "has root and LDAP admin (adm group)",
+    "ldap-admin": "has LDAP admin",
+    "root-adm": "has root (adm group)",
     "login-everywhere": "has SSH access everywhere (torproject group)",
     "gid-mismatch": "uid and gid do not match, might have extra access",
 }
@@ -106,7 +116,7 @@ def audit_ldap(con, user="*"):
     print("uid\tflags\tgroups")
     for dn, result in con.search_users(filter):
         logging.debug("dn: %s, dump: %s" % (dn, result))
-        info, flags = parse_ldap_result_user(dn, result)
+        info, flags = parse_ldap_result_user(con, dn, result)
         print(info)
         for flag in flags:
             logging.warning(
