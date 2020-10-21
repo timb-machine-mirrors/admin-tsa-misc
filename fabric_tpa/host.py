d21058bf14dad4e0d86bd800f170c355d81b3769
Antoine Beaupré <anarcat@debian.org>
2020-10-21 11:39:09 -0400

    show more useful info from remote on check

--- fabric_tpa/host.py
+++ fabric_tpa/host.py
@@ -540,7 +540,7 @@ def install_hetzner_robot(con,
     # STEP 1: logging is handled in the `install` script, in the
     # MatchingHostKeyPolicy class
     logging.info("STEP 1: fingerprint checked, making sure we have access")
-    con.run("true")
+    con.run("hostname ; uptime")
 
     hostname, _ = fqdn.split('.', 1)
     logging.info('STEP 2: setting hostname to %s', hostname)
