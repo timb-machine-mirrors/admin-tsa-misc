timb-machine-mirrors/admin-tsa-misc

commit 068503ce4ef843660d9ce3990c0508927bc0ee60

/boot is the boot *partition*, not device
This should fix the empty /boot problem in future installs.

fabric_tpa/host.py
@@ -590,7 +590,7 @@ def install_hetzner_robot(con,
     logging.info('mounting partitions from FAI')
     # TODO: parse the .sh file ourselves?
     # XXX: error handling?
-    res = con.run('. /tmp/fai/disk_var.sh && mkdir -p /target && mount "$ROOT_PARTITION" /target ; mkdir -p /target/boot && mount "$BOOT_DEVICE" /target/boot', warn=True)  # noqa: E501
+    res = con.run('. /tmp/fai/disk_var.sh && mkdir -p /target && mount "$ROOT_PARTITION" /target ; mkdir -p /target/boot && mount "$BOOT_PARTITION" /target/boot', warn=True)  # noqa: E501
     if res.failed:
         logging.warning("failed to mount partitions, assuming FAI did it")
 

commit 1b322d585bc3f1cc63e2f750a76fe016fe204e66

fallback to BOOT_DEVICE of BOOT_PARTITION is missing
Commit 068503ce introduced a bug in the Hetzner Robot installer where
/target/boot was always empty. I suspect this is because the patch
was made for the installer to work on the gnt-chi cluster, which uses
hardware RAID. Maybe what happens is that FAI gives us two different
variables depending on the configuration.

The commit that introduced this bug is:

    commit 068503ce4ef843660d9ce3990c0508927bc0ee60
    Author:     Antoine Beaupré <anarcat@debian.org>
    Date: Mon Nov 2 16:18:06 2020 -0500

    /boot is the boot *partition*, not device

    This should fix the empty /boot problem in future installs.

Now we still default to the BOOT_PARTITION, but fallback if the
variable isn't defined, which seems to be the case in our current
tests with the gnt-fsn-NVMe disk configuration.

fabric_tpa/host.py
@@ -615,7 +615,7 @@ def install_hetzner_robot(con,
     logging.info('mounting partitions from FAI')
     # TODO: parse the .sh file ourselves?
     # XXX: error handling?
-    res = con.run('. /tmp/fai/disk_var.sh && mkdir -p /target && mount "$ROOT_PARTITION" /target ; mkdir -p /target/boot && mount "$BOOT_PARTITION" /target/boot', warn=True)  # noqa: E501
+    res = con.run('. /tmp/fai/disk_var.sh && mkdir -p /target && mount "$ROOT_PARTITION" /target ; mkdir -p /target/boot && mount "${BOOT_PARTITION:-$BOOT_DEVICE}" /target/boot', warn=True)  # noqa: E501
     if res.failed:
         logging.warning("failed to mount partitions, assuming FAI did it")
 